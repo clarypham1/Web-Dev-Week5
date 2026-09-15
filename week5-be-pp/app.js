ed09db1b193b8dd41952f158e53763338ca281fc
Clary <claryphp@metropolia.fi>
2026-09-15 14:29:11 +0300

    chore: configure dotenv for environment management

--- week5-be-pp/app.js
+++ week5-be-pp/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const tourRouter = require("./routes/tourRouter");
--- week5-be-pp/config/db.js
+++ week5-be-pp/config/db.js
@@ -1,14 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/web-dev");
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected successfully');
   } catch (error) {
-    console.log(error);
+    console.error('MongoDB connection error:', error);
     process.exit(1);
   }
 };
- 
-module.exports = connectDB;
 
+module.exports = connectDB;
